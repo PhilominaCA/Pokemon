3004a0f229ecb2de01e0481a4c7c8c7a722c7a54
PhilominaCA <35137186+PhilominaCA@users.noreply.github.com>
2022-02-14 11:10:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -128,8 +128,8 @@ async function getPokeData(first, Sec) {
     catch (e) {
         console.log(e);
         let error_msg = document.createElement("h4");
-        error_msg.innerHTML = `Sorry Can't Fetch Any Pokemons right now <br/><br/>
-        Kindly access the URL : <a href="https://cors-anywhere.herokuapp.com/corsdemo">https://cors-anywhere.herokuapp.com/corsdemo </a>  ,<br/>
+        error_msg.innerHTML = `Sorry Can't Fetch Any Pokemons right now due to CORS error<br/><br/>
+        Kindly access the URL : <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank">https://cors-anywhere.herokuapp.com/corsdemo </a>  ,<br/>
         then click on the button <i>'request temporary access to the demo server'</i> button, then again try accessing the site`;
         document.body.appendChild(error_msg);
 
